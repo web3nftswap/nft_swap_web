300f106cd26165b58294d977ad13821f9fa7fc31
Fortitude-lan <358098351@qq.com>
2024-10-23 00:51:55 +0800

    Offer red dot

--- app/(root)/userCenter/page.tsx
+++ app/(root)/userCenter/page.tsx
@@ -304,9 +304,11 @@ const UserCenter = () => {
             }}
           >
             Offer
-            <div className="absolute inline-flex items-center justify-center w-3 h-3 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
-              {/* {offerCounts} */}
-            </div>
+            {offerCounts ? (
+              <div className="absolute inline-flex items-center justify-center w-3 h-3 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900"></div>
+            ) : (
+              ""
+            )}
           </Button>
           <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
             <SheetContent side="right" className="w-[80vw] bg-white">
