c0e4def524192278541f5ac9f18416c79a624d3a
Fortitude-lan <358098351@qq.com>
2024-11-11 15:06:03 +0800

    subnftas

--- app/(root)/consolidate/page.tsx
+++ app/(root)/consolidate/page.tsx
@@ -17,6 +17,7 @@ import { FaRegCircleCheck } from "react-icons/fa6";
 import { LuFileStack } from "react-icons/lu";
 import { RiErrorWarningLine } from "react-icons/ri";
 import { FiEdit } from "react-icons/fi";
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 
 const Consolidate = () => {
   const [mergeBtn, setmergeBtn] = useState(false);
@@ -24,14 +25,14 @@ const Consolidate = () => {
   const [datas, setdatas] = useState([]);
 
   const { api, allAccounts, injector, extensionEnabled, pending, setPending } =
-  useSubstrateContext();
+    useSubstrateContext();
 
   const { toast } = useToast();
 
   useEffect(() => {
-     fetchCollectionDatas();
+    fetchCollectionDatas();
   }, [api]);
-  const  fetchCollectionDatas = async () => {
+  const fetchCollectionDatas = async () => {
     if (!api) return; // 如果 api 尚未初始化，直接返回
 
     try {
@@ -51,12 +52,17 @@ const Consolidate = () => {
           );
           const nftMetaInfo = JSON.parse(hexCodeToString(metainfo).slice(1));
           // // console.log("nftMetaInfo", nftMetaInfo);
+          let subNfts = [];
+          if (status == "merged") {
+            subNfts = await getSubNfts(i);
+          }
           return {
             nft: i,
             status: status,
             url: nftMetaInfo.url,
             name: nftMetaInfo.name,
             desc: nftMetaInfo.desc,
+            subNfts,
           };
         })
       );
@@ -66,7 +72,32 @@ const Consolidate = () => {
       console.error("Error fetching collection IDs:", error);
     }
   };
-
+  const getInfo = async (id) => {
+    // 获取每一个集合的信息
+    const nftInfo = await api?.query.nftModule.nftCollections(id); // 使用 key.args[0] 作为参数
+    const [maxItem, curIndex, metainfo] = JSON.parse(JSON.stringify(nftInfo));
+    const nftMetaInfo = JSON.parse(hexCodeToString(metainfo).slice(1));
+    return nftMetaInfo;
+  };
+  const getSubNfts = async (nft: any) => {
+    // console.log("[Query] nftDetails");
+    // console.log(nft);
+    const nftDetails = await api?.query.nftModule.nftDetails([nft[0], nft[1]]);
+    let res: any = [];
+    const { mergedNft, subNfts, metadata } = JSON.parse(
+      JSON.stringify(nftDetails)
+    );
+    for (let i = 0; i < subNfts.length; i++) {
+      const details: any = await getInfo(subNfts[i][0]);
+      // console.log(details);
+      res.push({
+        nft: subNfts[i],
+        name: details.name,
+      });
+    }
+    // console.log("red", res);
+    return res;
+  };
   const getNftConsolidateStatus = async (
     collectionId: any,
     itemIndex: any
@@ -136,7 +167,7 @@ const Consolidate = () => {
           injector
         );
         // console.log(`mint hash: ${hash.toHex()}`);
-         fetchCollectionDatas();
+        fetchCollectionDatas();
         toast({
           title: (
             <div className="flex items-center">
@@ -204,7 +235,7 @@ const Consolidate = () => {
           injector
         );
         // console.log(`split hash: ${hash.toHex()}`);
-         fetchCollectionDatas();
+        fetchCollectionDatas();
         toast({
           title: (
             <div className="flex items-center">
@@ -238,7 +269,7 @@ const Consolidate = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden sm:px-10 px-5">
       <Header />
 
-      <div className="max-w-[80%] w-full relative  flex flex-col my-20  items-start justify-start">
+      <div className="max-w-[80%] w-full relative flex flex-col my-20  items-start justify-start">
         <div className="absolute left-0 z-20 flex items-center space-x-2 mt-20">
           <Input className="w-[200px]" />
           <Button className="">Search</Button>
@@ -274,6 +305,7 @@ const Consolidate = () => {
           )}
         </div>
         <div className="mt-40 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {/* <div className="mt-40 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"> */}
           {/* 遍历 Art 类别下的 NFT */}
           {datas
             .filter((item: any) => item.status !== "sub") // 先过滤掉状态为 "sub" 的项目
@@ -286,6 +318,7 @@ const Consolidate = () => {
                 splitBtn={splitBtn}
                 datas={datas}
                 setdatas={setdatas}
+                getSubNfts={getSubNfts}
               />
             ))}
         </div>
@@ -305,6 +338,7 @@ type DummyContentProps = {
   status: string;
   datas: any;
   setdatas: any;
+  getSubNfts: any;
 };
 const DummyContent: React.FC<DummyContentProps> = ({
   item,
@@ -312,6 +346,7 @@ const DummyContent: React.FC<DummyContentProps> = ({
   splitBtn,
   setdatas,
   status,
+  getSubNfts,
 }) => {
   return (
     <div className="cursor-pointer relative bg-white shadow-md rounded-t-lg rounded-b-md p-4 w-full max-w-sm mx-auto">
@@ -382,12 +417,41 @@ const DummyContent: React.FC<DummyContentProps> = ({
 
       {/* NFT Info */}
       <div className="mt-4 text-center">
-        <h3 className="text-xl text-black-100 font-semibold">{item.name} <a className="text-purple-300">#{item.nft[1]}</a></h3>
-        <p className="text-md text-black-100 my-2"> 
+        <h3 className="text-xl text-black-100 font-semibold">
+          {item.name} <a className="text-purple-300">#{item.nft[1]}</a>
+        </h3>
+        <p className="text-md text-black-100 my-2">
           {item.nft[0].slice(0, 6)}...{item.nft[0].slice(-4)}
         </p>
         {/* <p className="text-sm text-gray-500">idx：{item.nft[1]}</p> */}
         <p className="text-lg font-bold text-pink-500">{item.nft[2]}%</p>
+        {item.subNfts.length && (
+          <div className="text-sm text-gray-500 py-2 ">
+            <AnimatedTooltip
+              items={[
+                {
+                  id: 1,
+                  name: "SubNFTs",
+                  designation: (
+                    <div>
+                      {item.subNfts.map((i, idx) => (
+                        <p key={`owners-${idx}`}>
+                          <span>
+                            {i.name}{" "}
+                            <a className="text-purple-300 mr-2">#{i.nft[1]}</a>
+                          </span>
+                          <span>
+                            {i.nft[0].slice(0, 6)}...{i.nft[0].slice(-4)}
+                          </span>
+                        </p>
+                      ))}
+                    </div>
+                  ),
+                },
+              ]}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
